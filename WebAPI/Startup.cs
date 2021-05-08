7f01db9376bdd03b2d328286513387a0edd1a6da
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-08 18:25:38 +0300

    Added Cors!

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,6 +33,8 @@ namespace WebAPI
 
             //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+            services.AddCors();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -69,6 +71,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
