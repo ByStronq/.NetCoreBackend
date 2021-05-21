8e570b4ba91ab54aa97f570d43485e2c6e548fe9
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-21 04:57:25 +0300

    Unused using namespaces removed!

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -1,10 +1,8 @@
 ﻿using Castle.DynamicProxy;
 using Core.Aspects.Autofac.Performance;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace Core.Utilities.Interceptors
 {
@@ -17,7 +15,6 @@ namespace Core.Utilities.Interceptors
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-            //classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));
             classAttributes.Add(new PerformanceAspect(5));
 
             return classAttributes.OrderBy(x => x.Priority).ToArray();
